ae19888fbb723efee8ab7831ce068122d8e2dd3d
Anshun-raj <anshur61500@gmail.com>
2024-02-07 19:25:58 +0530

    First Commit

---
+++ hotel.reserve.com/src/main/java/hotel_reservation_system/hotel/reserve/com/App.java
@@ -0,0 +1,13 @@
+package hotel_reservation_system.hotel.reserve.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
